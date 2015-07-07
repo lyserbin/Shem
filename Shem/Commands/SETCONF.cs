9e4c2c1f6239700c2426aa8d52bdd599d5c08dc5
TyKonKet <matteoautieri@live.it>
2015-07-07 14:41:37 +0200

    Improved SETCONF

--- Shem/Commands/SETCONF.cs
+++ Shem/Commands/SETCONF.cs
@@ -1,28 +1,47 @@
 ﻿
+using System;
 namespace Shem.Commands
 {
     public class SETCONF : TCCommand
     {
-
-        private Configs configs;
-
-        private string value;
+        private Configs[] configs;
+        private string[] values;
 
         /// <summary>
         /// Change the value of one configuration variables.
         /// </summary>
-        /// <param name="configs"></param>
+        /// <param name="config"></param>
         /// <param name="value"></param>
-        public SETCONF(Configs configs, string value)
+        public SETCONF(Configs config, string value)
         {
-            this.configs = configs;
-
-            this.value = value;
+            this.configs = new Configs[] { config };
+            this.values = new string[] { value };
         }
+
+        //TODO: Find a solution to set multiple parameters
+        /// <summary>
+        /// Change the values of one or more configuration variables.
+        /// </summary>
+        /// <param name="configs"></param>
+        /// <param name="values"></param>
+        //public SETCONF(params Configs[] configs, params string[] values)
+        //{
+        //    this.configs = configs;
+        //    this.values = values;
+        //}
 
         public override string Raw()
         {
-            return string.Format("SETCONF {0}=\"{1}\"\r\n", configs.ToString(), value);
+            string command = (new Func<string>(() =>
+            {
+                string ks = "";
+                for (var i = 0; i < configs.Length; i++)
+                {
+                    ks += string.Format(" {0}=\"{1}\"", configs[i], values[i]);
+                }
+                return ks;
+            }))();
+            return string.Format("SETCONF{0}\r\n", command);
         }
     }
 }
