a5fc2d1ced2fd19fd850c2f09d5f478211187f46
TyKonKet <matteoautieri@live.it>
2015-07-07 13:38:30 +0200

    Forgot SETCONF.cs

---
+++ Shem/Commands/SETCONF.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Shem.Commands
+{
+    public class SETCONF : TCCommand
+    {
+        /// <summary>
+        /// Configurations availables for SETCONF command.
+        /// </summary>
+        public enum Configs
+        {
+            SocksPort,
+
+            SocksPolicy,
+
+            MaxMemInQueues,
+
+            Log,
+
+            RunAsDaemon,
+
+            DataDirectory,
+
+            ControlPort,
+
+            HashedControlPassword,
+
+            CookieAuthentication,
+
+            HiddenServiceDir,
+
+            HiddenServicePort,
+
+            ORPort,
+
+            Address,
+
+            OutboundBindAddress,
+
+            Nickname,
+
+            RelayBandwidthRate,
+
+            RelayBandwidthBurst,
+
+            AccountingMax,
+
+            AccountingStart,
+
+            ContactInfo,
+
+            DirPort,
+
+            DirPortFrontPage,
+
+            MyFamily,
+
+            ExitPolicy,
+
+            BridgeRelay,
+
+            PublishServerDescriptor
+        }
+
+        private Configs configs;
+
+        private string value;
+
+        /// <summary>
+        /// Change the value of one configuration variables.
+        /// </summary>
+        /// <param name="configs"></param>
+        /// <param name="value"></param>
+        public SETCONF(Configs configs, string value)
+        {
+            this.configs = configs;
+
+            this.value = value;
+        }
+
+        public override string Raw()
+        {
+            return string.Format("SETCONF {0}=\"{1}\"\r\n", configs.ToString(), value);
+        }
+    }
+}
