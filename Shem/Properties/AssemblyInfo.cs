db650f3efcb464868d197b21bec5f7aae154f2d1
TyKonKet <matteoautieri@live.it>
2015-07-07 14:54:25 +0200

    Modified some project settings and switched to .NET 4.5.1

--- Shem/Properties/AssemblyInfo.cs
+++ Shem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sharp-stem")]
-[assembly: AssemblyDescription(".NET Stem")]
+[assembly: AssemblyTitle("Shem")]
+[assembly: AssemblyDescription(".NET tor control lib")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LucianSoft")]
-[assembly: AssemblyProduct("sharp-stem")]
+[assembly: AssemblyProduct("Shem")]
 [assembly: AssemblyCopyright("Copyleft © LucianSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a74581d7-c1b2-4eb3-b45b-aa108d298a80")]
