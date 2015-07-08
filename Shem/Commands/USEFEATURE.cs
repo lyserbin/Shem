6f1dee209dd52ab0817184a65f8b8b359d2b4294
TyKonKet <matteoautieri@live.it>
2015-07-08 15:34:29 +0200

    Implemented USEFEATURE command

--- Shem/Commands/USEFEATURE.cs
+++ Shem/Commands/USEFEATURE.cs
@@ -1,23 +1,25 @@
-﻿using System;
-
+﻿
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    /// Adding additional features to the control protocol sometimes will break backwards compatibility. Initially such features are added into Tor and disabled by default. USEFEATURE can enable these additional features. Once enabled, a feature stays enabled for the duration of the connection to the controller. A new connection to the controller must be opened to disable an enabled feature.
     /// </summary>
     public class USEFEATURE : TCCommand
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public USEFEATURE()
-        {
+        private string featureName;
 
+        /// <summary>
+        /// Adding additional features to the control protocol sometimes will break backwards compatibility. Initially such features are added into Tor and disabled by default. USEFEATURE can enable these additional features. Once enabled, a feature stays enabled for the duration of the connection to the controller. A new connection to the controller must be opened to disable an enabled feature.
+        /// </summary>
+        /// <param name="featureName"></param>
+        public USEFEATURE(string featureName)
+        {
+            this.featureName = featureName;
         }
 
         public override string Raw()
         {
-            throw new NotImplementedException();
+            return string.Format("USEFEATURE {0}\r\n", featureName);
         }
     }
 }
