947d3efbb2e1ebb6b43616753810a46baaff309e
TyKonKet <matteoautieri@live.it>
2015-07-08 15:39:24 +0200

    Implemented VERBOSE_NAMES command

--- Shem/Commands/VERBOSE_NAMES.cs
+++ Shem/Commands/VERBOSE_NAMES.cs
@@ -3,7 +3,7 @@
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    /// Replaces ServerID with LongName in events and GETINFO results. LongName provides a Fingerprint for all routers, an indication of Named status, and a Nickname if one is known. LongName is strictly more informative than ServerID, which only provides either a Fingerprint or a Nickname.
     /// </summary>
     public class VERBOSE_NAMES : TCCommand
     {
@@ -12,7 +12,7 @@ namespace Shem.Commands
         /// </summary>
         public VERBOSE_NAMES()
         {
-
+            //TODO: Implement this, currently not documented in control-spec-otr-0.2.6.9.txt
         }
 
         public override string Raw()
