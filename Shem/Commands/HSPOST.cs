bcc6a4bf5332662c18554d2aff690af7733f7cd3
TyKonKet <matteoautieri@live.it>
2015-07-08 18:11:27 +0200

    Implemented HSPOST command

--- Shem/Commands/HSPOST.cs
+++ Shem/Commands/HSPOST.cs
@@ -1,23 +1,33 @@
-﻿using System;
-
+﻿
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    ///  This command launches a hidden service descriptor upload to the specified HSDirs. If one or more Server arguments are provided, an upload is triggered on each of them in parallel. If no Server options are provided, it behaves like a normal HS descriptor upload and will upload to the set of responsible HS directories.
     /// </summary>
     public class HSPOST : TCCommand
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public HSPOST()
-        {
+        private string[] servers;
+        private string descriptor;
 
+        /// <summary>
+        /// This command launches a hidden service descriptor upload to the specified HSDirs. If one or more Server arguments are provided, an upload is triggered on each of them in parallel. If no Server options are provided, it behaves like a normal HS descriptor upload and will upload to the set of responsible HS directories.
+        /// </summary>
+        /// <param name="servers">The LongName (Fingerprint [~/=Nickname]) of the servers.</param>
+        /// <param name="descriptor">The text of the descriptor formatted as specified in rend-spec.txt section 1.3.</param>
+        public HSPOST(string descriptor, params string[] servers)
+        {
+            this.servers = servers;
+            this.descriptor = descriptor;
         }
 
         public override string Raw()
         {
-            throw new NotImplementedException();
+            string servers_formatted = "";
+            foreach (var s in servers)
+            {
+                servers_formatted += " SERVER=" + s;
+            }
+            return string.Format("+HSPOST{0}\r\n{1}\r\n.\r\n", servers_formatted, descriptor);
         }
     }
 }
