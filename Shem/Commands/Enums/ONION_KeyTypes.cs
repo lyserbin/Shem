d2387f7a38bdf8a2ed64b9882a2db138cc70719d
TyKonKet <matteoautieri@live.it>
2015-07-08 17:58:28 +0200

    Forgot ONION_KeyTypes enum

---
+++ Shem/Commands/Enums/ONION_KeyTypes.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shem.Commands.Enums
+{
+    public enum ONION_KeyTypes
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        NEW,
+
+        /// <summary>
+        /// The server should use the 1024 bit RSA key provided in as KeyBlob</summary>
+        RSA1024
+    }
+}
