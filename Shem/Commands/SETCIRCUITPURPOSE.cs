20bfaec0233b2e51711a6de88e78c0fcff52b4c5
TyKonKet <matteoautieri@live.it>
2015-07-08 13:08:29 +0200

    Implemented SETCIRCUITPURPOSE command

--- Shem/Commands/SETCIRCUITPURPOSE.cs
+++ Shem/Commands/SETCIRCUITPURPOSE.cs
@@ -1,23 +1,28 @@
-﻿using System;
-
+﻿
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    /// This changes the circuit's purpose.
     /// </summary>
     public class SETCIRCUITPURPOSE : TCCommand
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public SETCIRCUITPURPOSE()
-        {
+        private string circuitID;
+        private string purpose;
 
+        /// <summary>
+        /// This changes the circuit's purpose.
+        /// </summary>
+        /// <param name="circuitID"></param>
+        /// <param name="purpose"></param>
+        public SETCIRCUITPURPOSE(string circuitID, string purpose = "")
+        {
+            this.circuitID = circuitID;
+            this.purpose = purpose;
         }
 
         public override string Raw()
         {
-            throw new NotImplementedException();
+            return string.Format("SETCIRCUITPURPOSE {0} purpose={1}", circuitID, purpose);
         }
     }
 }
