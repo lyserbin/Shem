10d35574bf46a5c590f8ceada290572ec6982499
TyKonKet <matteoautieri@live.it>
2015-07-08 15:30:33 +0200

    Implemented CLOSECIRCUIT command

---
+++ Shem/Commands/Enums/CLOSECIRCUIT_Flags.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Shem.Commands.Enums
+{
+    public enum CLOSECIRCUIT_Flags
+    {
+        /// <summary>
+        /// If "IfUnused" is provided, do not close the circuit unless it is unused.
+        /// </summary>
+        IfUnused
+    }
+}
