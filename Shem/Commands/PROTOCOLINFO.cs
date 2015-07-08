755d338d8804dff296609aeb069324ed2dc581b1
TyKonKet <matteoautieri@live.it>
2015-07-08 17:04:05 +0200

    Implemented PROTOCOLINFO command

--- Shem/Commands/PROTOCOLINFO.cs
+++ Shem/Commands/PROTOCOLINFO.cs
@@ -1,23 +1,25 @@
-﻿using System;
-
+﻿
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    /// Returns all information about the protocol. PROTOCOLINFO may be used (but only once!) before AUTHENTICATE.
     /// </summary>
     public class PROTOCOLINFO : TCCommand
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public PROTOCOLINFO()
-        {
+        private string PIVersion;
 
+        /// <summary>
+        /// Returns all information about the protocol. PROTOCOLINFO may be used (but only once!) before AUTHENTICATE.
+        /// </summary>
+        /// <param name="PIVersion">PIVERSION is there in case we drastically change the syntax one day. For now it should always be "1".  Controllers MAY provide a list of the protocolinfo versions they support; Tor MAY select a version that the controller does not support.</param>
+        public PROTOCOLINFO(string PIVersion = "1")
+        {
+            this.PIVersion = PIVersion;
         }
 
         public override string Raw()
         {
-            throw new NotImplementedException();
+            return string.Format("PROTOCOLINFO {0}\r\n", PIVersion);
         }
     }
 }
