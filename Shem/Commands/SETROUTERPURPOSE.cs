cecbaab6e7af3fddba59336d8c4c60d373471b67
TyKonKet <matteoautieri@live.it>
2015-07-08 13:10:55 +0200

    Implemented SETROUTERPURPOSE command

--- Shem/Commands/SETROUTERPURPOSE.cs
+++ Shem/Commands/SETROUTERPURPOSE.cs
@@ -3,12 +3,12 @@
 namespace Shem.Commands
 {
     /// <summary>
-    /// 
+    /// This command was disabled and made obsolete as of Tor 0.2.0.8-alpha. It doesn't exist anymore, and is listed here only for historical interest.
     /// </summary>
     public class SETROUTERPURPOSE : TCCommand
     {
         /// <summary>
-        /// 
+        /// This command was disabled and made obsolete as of Tor 0.2.0.8-alpha. It doesn't exist anymore, and is listed here only for historical interest.
         /// </summary>
         public SETROUTERPURPOSE()
         {
