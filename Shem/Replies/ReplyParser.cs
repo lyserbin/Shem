e772d4bfa66a4bf31355c27c8cd337f8e5e1b37a
lyserbin <lyserbin@luciansoft.ml>
2015-07-10 01:56:23 +0200

    ReplyParser now using StringBuilder

--- Shem/Replies/ReplyParser.cs
+++ Shem/Replies/ReplyParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using Shem.Exceptions;
 using Shem.Utils;
+using System.Text;
 
 namespace Shem.Replies
 {
@@ -15,7 +16,7 @@ namespace Shem.Replies
         {
             int tmpcode;
             ReplyCodes code;
-            string replyline = "";
+            StringBuilder replyline = new StringBuilder();
 
             if ((rawstring.Length < i + 3) || (!int.TryParse(rawstring.Substring(i, i + 3), out tmpcode)))
                 throw new NullReplyCodeException(rawstring, i); // if the current reply is < 3 chars or has not a code
@@ -29,14 +30,14 @@ namespace Shem.Replies
                         Logger.LogWarn(string.Format("Reply code not identified \"{0}\" in \"{1}\"", tmpcode, rawstring.Replace("\r\n", "\\r\\n")));
 
                     code = (ReplyCodes)tmpcode;
-                    current.Add(new Reply(code, replyline, rawstring)); // Add the reply to the return collection
+                    current.Add(new Reply(code, replyline.ToString(), rawstring)); // Add the reply to the return collection
                     if (i + 2 == rawstring.Length) // we are at the end of the string (if it is a multi response one)
                         return;
                     else // another ride BABY
                         rparse(rawstring, i + 2, ref current); // We are not at the end of the string (if it is a multi response one)
                 }
 
-                replyline += rawstring[i++];
+                replyline.Append(rawstring[i++]);
             }
         }
 
