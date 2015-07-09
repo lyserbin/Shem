0a1a9556831a3de9620f906d50c8b0ad5e66d1a4
TyKonKet <matteoautieri@live.it>
2015-07-09 15:10:08 +0200

    Implemented SETEVENTS and EXTENDED_EVENTS commands

---
+++ Shem/Commands/Enums/AsyncEvents.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Shem.Commands
+{
+    public enum AsyncEvents
+    {
+        ADDRMAP,
+        AUTHDIR_NEWDESCS,
+        BUILDTIMEOUT_SET,
+        BW,
+        CIRC,
+        CIRC_MINOR,
+        CLIENTS_SEEN,
+        CONF_CHANGED,
+        DEBUG,
+        DESCCHANGED,
+        ERR,
+        GUARD,
+        INFO,
+        NEWCONSENSUS,
+        NEWDESC,
+        NOTICE,
+        NS,
+        ORCONN,
+        SIGNAL,
+        STATUS_CLIENT,
+        STATUS_GENERAL,
+        STATUS_SERVER,
+        STREAM,
+        STREAM_BW,
+        WARN,
+        STATUS_SEVER
+    }
+}
