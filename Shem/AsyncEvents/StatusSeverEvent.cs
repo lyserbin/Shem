331ed83a1559da69cebd394af625e7f16a6e431a
TyKonKet <matteoautieri@live.it>
2015-07-13 20:30:50 +0200

    Fixed StatusSever event

--- Shem/AsyncEvents/StatusSeverEvent.cs
+++ Shem/AsyncEvents/StatusSeverEvent.cs
@@ -1,33 +1,11 @@
-﻿using Shem.Replies;
-
+﻿
 namespace Shem.AsyncEvents
 {
-    public class StatusSeverEvent : TorEvent
+    public class StatusSeverEvent : StatusServerEvent
     {
         public StatusSeverEvent()
         {
 
-        }
-
-        public override TorEvents Event
-        {
-            get { return TorEvents.STATUS_SEVER; }
-        }
-
-        protected override void ParseToEvent(Reply reply)
-        {
-            //TODO: Implement parsing
-            throw new System.NotImplementedException();
-        }
-
-        public override string RawString
-        {
-            get { throw new System.NotImplementedException(); }
-        }
-
-        public override string EventLine
-        {
-            get { throw new System.NotImplementedException(); }
         }
     }
 }
