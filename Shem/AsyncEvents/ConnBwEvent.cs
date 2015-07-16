4fa98dc339cb2b3cc52b35e2f9ad3a1b236d5daf
TyKonKet <matteoautieri@live.it>
2015-07-16 18:26:04 +0200

    Implemented ConnBw event

--- Shem/AsyncEvents/ConnBwEvent.cs
+++ Shem/AsyncEvents/ConnBwEvent.cs
@@ -1,4 +1,5 @@
 ﻿
+using Shem.Utils;
 namespace Shem.AsyncEvents
 {
     public class ConnBwEvent : TorEvent
@@ -13,9 +14,77 @@ namespace Shem.AsyncEvents
             get { return TorEvents.CONN_BW; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ID
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ConnBwTypes Type
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int BytesRead
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int BytesWritten
+        {
+            get;
+            protected set;
+        }
+
         protected override void ParseToEvent(Replies.Reply reply)
         {
             base.ParseToEvent(reply);
+
+            int index = 0;
+            string[] split = EventLine.Split(' ');
+
+            for (int i = index; i < split.Length; i++)
+            {
+                if (split[i].Contains("="))
+                {
+                    string key = split[i].Split('=')[0];
+                    string value = split[i].Split('=')[1];
+
+                    switch (key.ToUpper())
+                    {
+                        case "ID":
+                            ID = value;
+                            break;
+                        case "TYPE":
+                            Type = Utility.ParseEnum<ConnBwTypes>(value);
+                            break;
+                        case "READ":
+                            int r = 0;
+                            int.TryParse(value, out r);
+                            BytesRead = r;
+                            break;
+                        case "WRITTEN":
+                            int w = 0;
+                            int.TryParse(value, out w);
+                            BytesWritten = w;
+                            break;
+                    }
+                }
+            }
         }
     }
 }
---
+++ Shem/AsyncEvents/Enums/ConnBwTypes.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Shem.AsyncEvents
+{
+    public enum ConnBwTypes
+    {
+        /// <summary>
+        /// Carrying traffic within the tor network. This can either be our own (client) traffic or traffic we're relaying within the network.
+        /// </summary>
+        OR,
+
+        /// <summary>
+        /// Fetching tor descriptor data, or transmitting descriptors we're mirroring.
+        /// </summary>
+        DIR,
+
+        /// <summary>
+        /// Carrying traffic between the tor network and an external destination.
+        /// </summary>
+        EXIT
+    }
+}
