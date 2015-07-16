06efdcf43040ad9b6e9a721dc20031e8a3cc1c82
TyKonKet <matteoautieri@live.it>
2015-07-16 18:04:49 +0200

    Implemented TransportLaunched event

--- Shem/AsyncEvents/TransportLaunchedEvent.cs
+++ Shem/AsyncEvents/TransportLaunchedEvent.cs
@@ -13,9 +13,55 @@ namespace Shem.AsyncEvents
             get { return TorEvents.TRANSPORT_LAUNCHED; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Type
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Address
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Port
+        {
+            get;
+            protected set;
+        }
+
         protected override void ParseToEvent(Replies.Reply reply)
         {
             base.ParseToEvent(reply);
+
+            string[] split = EventLine.Split(' ');
+
+            Type = split[0];
+
+            Name = split[1];
+
+            Address = split[2];
+
+            Port = split[3];
         }
     }
 }
