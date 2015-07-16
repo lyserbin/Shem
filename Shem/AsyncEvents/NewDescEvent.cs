90372f16ab2d845b24b48a751c971bbf170f4323
TyKonKet <matteoautieri@live.it>
2015-07-16 15:53:34 +0200

    Implemented New Desc event

--- Shem/AsyncEvents/NewDescEvent.cs
+++ Shem/AsyncEvents/NewDescEvent.cs
@@ -1,4 +1,5 @@
-﻿using Shem.Replies;
+﻿using System.Collections.Generic;
+using Shem.Replies;
 
 namespace Shem.AsyncEvents
 {
@@ -14,9 +15,18 @@ namespace Shem.AsyncEvents
             get { return TorEvents.NEWDESC; }
         }
 
+        public List<string> ServerIDs
+        {
+            get;
+            protected set;
+        }
+
         protected override void ParseToEvent(Reply reply)
         {
             base.ParseToEvent(reply);
+
+            ServerIDs = new List<string>();
+            ServerIDs.AddRange(EventLine.Split(' '));
         }
     }
 }
