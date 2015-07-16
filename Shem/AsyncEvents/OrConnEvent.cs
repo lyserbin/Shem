cf2628bf8a0048b87f054c5f27adfb3a53f53b29
TyKonKet <matteoautieri@live.it>
2015-07-16 15:36:15 +0200

    Implemented OrConn event

--- Shem/AsyncEvents/OrConnEvent.cs
+++ Shem/AsyncEvents/OrConnEvent.cs
@@ -1,4 +1,5 @@
 ﻿using Shem.Replies;
+using Shem.Utils;
 
 namespace Shem.AsyncEvents
 {
@@ -14,9 +15,85 @@ namespace Shem.AsyncEvents
             get { return TorEvents.ORCONN; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Target
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public OrConnStatus Status
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public OrConnReasons Reason
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CircuitsCount
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ID
+        {
+            get;
+            protected set;
+        }
+
         protected override void ParseToEvent(Reply reply)
         {
             base.ParseToEvent(reply);
+
+            int index = 0;
+            string[] split = EventLine.Split(' ');
+
+            Target = split[index];
+            index++;
+
+            Status = Utility.ParseEnum<OrConnStatus>(split[index]);
+            index++;
+
+            for (int i = index; i < split.Length; i++)
+            {
+                if (split[i].Contains("="))
+                {
+                    string key = split[i].Split('=')[0];
+                    string value = split[i].Split('=')[1];
+
+                    switch (key.ToUpper())
+                    {
+                        case "REASON":
+                            Reason = Utility.ParseEnum<OrConnReasons>(value);
+                            break;
+                        case "NCIRCS":
+                            CircuitsCount = int.Parse(value);
+                            break;
+                        case "ID":
+                            ID = value;
+                            break;
+                    }
+                }
+            }
         }
     }
 }
---
+++ Shem/Utils/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shem.Utils
+{
+    public static class Utility
+    {
+        public static T ParseEnum<T>(string value) where T : struct , IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type");
+            }
+
+            T tmp = default(T);
+            if (!Enum.TryParse<T>(value, true, out tmp))
+            {
+                Logger.LogWarn(string.Format("Error during the parsing of {0}.", typeof(T).ToString()));
+            }
+            return tmp;
+        }
+    }
+}
