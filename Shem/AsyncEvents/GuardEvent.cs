ef027f33857643821c3df27669483b0d8a82e0cd
TyKonKet <matteoautieri@live.it>
2015-07-16 16:57:22 +0200

    Implemented Guard event

--- Shem/AsyncEvents/GuardEvent.cs
+++ Shem/AsyncEvents/GuardEvent.cs
@@ -2,11 +2,41 @@
 
 namespace Shem.AsyncEvents
 {
+    /// <summary>
+    /// Our set of guard nodes has changed
+    /// </summary>
     public class GuardEvent : TorEvent
     {
         public GuardEvent()
         {
 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Type
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// Identifies the guard affected
+        /// </summary>
+        public string Name
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Status
+        {
+            get;
+            protected set;
         }
 
         public override TorEvents Event
@@ -17,6 +47,14 @@ namespace Shem.AsyncEvents
         protected override void ParseToEvent(Reply reply)
         {
             base.ParseToEvent(reply);
+
+            string[] split = EventLine.Split(' ');
+
+            Type = split[0];
+
+            Name = split[1];
+
+            Status = split[2];
         }
     }
 }
